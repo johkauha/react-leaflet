545866004ad4aecd8544a4b8010ddb11524b444e
ZMaster <z1master@hotmail.com>
2018-11-22 15:52:10 +0200

    Add semicircle

---
+++ src/Semicircle.js
@@ -0,0 +1,35 @@
+// @flow
+
+import { Semicircle as LeafletSemicircle } from 'leaflet-semicircle'
+
+import { withLeaflet } from './context'
+import Path from './Path'
+import type { LatLng, MapLayerProps, PathOptions } from './types'
+
+type LeafletElement = LeafletSemicircle
+type Props = {
+  center: LatLng,
+  radius: number,
+  startAngle: number,
+  stopAngle: number,
+} & MapLayerProps &
+  PathOptions &
+  Object
+
+class Semicircle extends Path<LeafletElement, Props> {
+  createLeafletElement(props: Props): LeafletElement {
+    const { center, radius, startAngle, stopAngle, ...options } = props
+    return new LeafletSemicircle(center, radius, startAngle, stopAngle, this.getOptions(options))
+  }
+
+  updateLeafletElement(fromProps: Props, toProps: Props) {
+    if (toProps.center !== fromProps.center) {
+      this.leafletElement.setLatLng(toProps.center)
+    }
+    if (toProps.radius !== fromProps.radius) {
+      this.leafletElement.setRadius(toProps.radius)
+    }
+  }
+}
+
+export default withLeaflet(Semicircle)
